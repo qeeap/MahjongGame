71ee2bc72e83ac7ebf5fe93ee68e9f2694bec76f
qeeap <fomicheva.lera06@gmail.com>
2026-03-06 23:48:32 +0300

    Initial commit: project structure

---
+++ src/main/java/com/mahjong/app/Main.java
@@ -0,0 +1,8 @@
+package com.mahjong.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Маджонг проект инициализирован!");
+        System.out.println("Структура папок создана правильно.");
+    }
+}
