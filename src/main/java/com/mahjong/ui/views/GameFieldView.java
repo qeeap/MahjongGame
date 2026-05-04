0d20160e62698978de06c7dc64ae7ad9b8a8429f
IconAiko <angelinalvova035@gmail.com>
2026-05-04 20:18:38 +0300

    добавила поле уровней. улучшила расположение файлов.

--- src/main/java/com/mahjong/ui/views/GameFieldView.java
+++ src/main/java/com/mahjong/ui/views/GameFieldView.java
@@ -9,6 +9,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 public class GameFieldView extends Pane {
 
     private ImageView backButton;
+    private Rectangle gameBoardRect;
     private static final double TILE_WIDTH = 60;
     private static final double TILE_HEIGHT = 80;
 
@@ -42,6 +44,13 @@ public class GameFieldView extends Pane {
             System.err.println("Кнопка 'Назад' не загружена: /images/back.png");
             backButton = new ImageView();
         }
+
+        gameBoardRect = new Rectangle();
+        gameBoardRect.setWidth(screenWidth * 0.85);
+        gameBoardRect.setHeight(screenHeight * 0.9);
+        gameBoardRect.setArcWidth(50);
+        gameBoardRect.setArcHeight(50);
+        gameBoardRect.setFill(Color.rgb(11, 77, 106, 0.9));
     }
 
     private void positionUI() {
@@ -53,7 +62,10 @@ public class GameFieldView extends Pane {
         backButton.setTranslateX(screenWidth / 30);
         backButton.setTranslateY(screenHeight / 30);
 
-        getChildren().add(backButton);
+        gameBoardRect.setTranslateX((screenWidth - gameBoardRect.getWidth()) / 2);
+        gameBoardRect.setTranslateY((screenHeight - gameBoardRect.getHeight()) / 2);
+
+        getChildren().addAll( gameBoardRect, backButton);
     }
 
     public ImageView getBackButton() { return backButton; }
